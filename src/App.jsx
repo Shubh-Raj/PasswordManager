0f3644a4221a33f944f74411c9af01737a89d2d9
Shubh-Raj <shubhraj625@gmail.com>
2025-03-24 23:11:07 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './components.js/Navbar'
+import Manager from './components.js/Manager'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>ui</div>
+      <Navbar />
+      <Manager/>
     </>
   )
 }
